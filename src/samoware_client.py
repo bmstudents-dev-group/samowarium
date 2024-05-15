99047fafb967653016720c01852697967cd41de5
Maksim <maxus2004123@gmail.com>
2024-05-15 16:18:54 +0300

    strip text in tags to remove extra spaces

--- src/samoware_client.py
+++ src/samoware_client.py
@@ -353,7 +353,7 @@ def setSessionInfo(context: SamowareContext) -> None:
 
 def htmlElementToText(element):
     if isinstance(element, bs4.NavigableString):
-        return html.escape(str(element).replace("\n", " "))
+        return html.escape(str(element).replace("\n", " ").strip())
     elif isinstance(element, bs4.Tag):
         if element.name == "a" and "href" in element.attrs:
             href = element["href"]
