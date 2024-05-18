54fa48f5b2e41aeefe7cbc7cbc7b48b0528ca209
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-05-18 15:17:07 +0300

    make auto init db

--- src/create_db.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-
-db = sqlite3.connect("database.db", check_same_thread=False)
-
-db.execute("CREATE TABLE clients(telegram_id PRIMARY KEY, samoware_context)")
--- src/samowarium.py
+++ src/samowarium.py
@@ -96,6 +96,7 @@ async def deactivate(telegram_id: int) -> None:
 
 async def main() -> None:
     logging.info("loading clients...")
+    database.init()
     for client in database.getAllClients():
         samoware_context = database.getSamowareContext(client[0])
         startSamowareLongPolling(client[0], samoware_context)
