bf7f8e3b5067aa58230651ab7c26e1d1e5efa961
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-05-23 17:37:30 +0300

    add env file with env vars managment

---
+++ src/env.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PROFILE_VAR_NAME = "ENV"
+VERSION_VAR_NAME = "VERSION"
+TG_TOKEN_VAR_NAME = "TELEGRAM_TOKEN"
+
+DEV_PROFILE_NAME = "DEV"
+PROD_PROFILE_NAME = "PROD"
+
+def getProfile():
+    return os.environ.get(PROFILE_VAR_NAME, DEV_PROFILE_NAME)
+
+def getVersion():
+    return os.environ.get(VERSION_VAR_NAME, "none")
+
+def getTelegramToken():
+    if TG_TOKEN_VAR_NAME not in os.environ:
+        raise EnvironmentError(f"{TG_TOKEN_VAR_NAME} env var does not exist")
+    return os.environ.get(TG_TOKEN_VAR_NAME)
+
+def isDevProfile():
+    return getProfile() == DEV_PROFILE_NAME
+
+def isProdProfile():
+    return getProfile() == PROD_PROFILE_NAME
