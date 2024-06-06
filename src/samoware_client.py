825fd6f47fb873a3c2957bb9d0bf001a29377cc7
Maksim <maxus2004123@gmail.com>
2024-06-07 01:03:00 +0300

    fix code style again

--- src/samoware_client.py
+++ src/samoware_client.py
@@ -318,10 +318,9 @@ def getInboxUpdates(context: SamowareContext) -> list:
                 mail["from_name"] = element.find("E-From").attrib["realName"]
             else:
                 mail["from_name"] = element.find("E-From").text
-            if (
-                (element.find("Subject") is not None) and
-                (element.find("Subject").text is not None)
-            ):
+            subjectExists = element.find("Subject") is not None
+            subjectTextExists = element.find("Subject").text is not None
+            if subjectTextExists and subjectTextExists:
                 mail["subject"] = html.escape(element.find("Subject").text)
             else:
                 mail["subject"] = "Письмо без темы"
