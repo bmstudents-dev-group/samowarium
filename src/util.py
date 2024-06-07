2c230f5ef41252838fbf9ad1699bf32bb3f69115
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-06-07 21:19:18 +0300

    remove unused retry_on_exception

--- src/util.py
+++ src/util.py
@@ -11,25 +11,3 @@ MessageSender = Callable[
 def make_dir_if_not_exist(path):
     if not os.path.exists(path):
         os.makedirs(path)
-
-
-async def retry_on_exception(
-    func: Awaitable[Callable[[], None]],
-    delay: int,
-    on_success: Callable[[], None],
-    on_failure: Callable[[Exception], None],
-    retry_count: int | None = None,
-) -> bool:
-    retries = 0
-    is_executed = False
-    while not is_executed and retries < retry_count:
-        try:
-            await func()
-            is_executed = True
-            on_success()
-            return True
-        except Exception as e:
-            on_failure(e)
-            retries += 1
-            await asyncio.sleep(delay)
-    return False
