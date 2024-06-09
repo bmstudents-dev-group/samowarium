d542b1df03581d22b4e45eff80f145d85e860946
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-06-09 14:44:00 +0300

    minor refactor env.py and util.py

--- src/util.py
+++ src/util.py
@@ -2,6 +2,7 @@ from http.client import HTTPResponse
 import os
 import subprocess
 from typing import Awaitable, Callable, Optional
+import logging as log
 
 MessageSender = Callable[
     [int, str, str, Optional[list[tuple[HTTPResponse, str]]]], Awaitable[None]
@@ -10,7 +11,9 @@ MessageSender = Callable[
 
 def make_dir_if_not_exist(path):
     if not os.path.exists(path):
+        log.debug(f"creates dir {path}")
         os.makedirs(path)
 
 def run_migrations():
+    log.debug("running migrations...")
     subprocess.run(["yoyo", "apply", "-b"])
