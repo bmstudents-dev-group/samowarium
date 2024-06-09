0267ad8147b51e8902798d957eb3d5bb593e6a93
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-06-09 15:03:41 +0300

    update tg bot file for passwords support

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,37 +1,53 @@
 from http.client import HTTPResponse
 from telegram import Update, InputMediaDocument
 import telegram
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler
 import logging as log
 from typing import Optional
 import asyncio
 from client_handler import ClientHandler
-from const import TELEGRAM_SEND_RETRY_DELAY_SEC
+from const import MARKDOWN_FORMAT, TELEGRAM_SEND_RETRY_DELAY_SEC
 from database import Database
 import env
 
 START_PROMPT = "Выдать доступ боту до почты :\n/login _логин_ _пароль_\n\nОтозвать доступ:\n/stop\n\nFAQ:\n/about"
 STOP_PROMPT = "Доступ отозван. Логин и сессия были удалены."
 ABOUT_PROMPT = """
-    Samowarium - бот, который пересылает входящие письма в личные сообщения телеграм.
+Samowarium - бот, который пересылает входящие письма в личные сообщения телеграм.
 
-    Список команд бота:
-    /login - выдать боту доступ к почтовому серверу.
-    /stop - отозвать доступ и удалить информацию о пользователе.
-    /about - получить дополнительную информацию.
+Список команд бота:
+/login - выдать боту доступ к почтовому серверу;
+/stop - отозвать доступ и удалить информацию о пользователе;
+/about - получить дополнительную информацию.
 
-    Как это работает?
-    При передаче пары логин/пароль бот получает от почтового сервера токен сессии, с помощью которого в дальнейшем обрабатывает входящие письма.
-    Бот не хранит пароли пользователей, а лишь использует их однократно во время авторизации для получения токена сессии, после чего их забывает. Токен сессии почтового сервера возможно использовать только для работы с почтой, бот не может с помощью него получить доступ к остальным сервисам МГТУ.
+Как это работает?
+При передаче пары логин/пароль бот получает от почтового сервера токен сессии, с помощью которого в дальнейшем обрабатывает входящие письма.
+Если пользователь выбрал опцию "не сохранять пароль", то бот его забывает после получения сессии почтового сервера. В ином случае бот запоминает пароль пользователя.
+Токен сессии почтового сервера возможно использовать только для работы с почтой, бот не может с помощью него получить доступ к остальным сервисам МГТУ.
 
-    Версия: `{}-{}`
+Как храняться пароли?
+Пароли пользователей храняться в защищенной базе данных, доступ к которой имеют только разработчики бота и робот github actions.
+Каждый пароль хранится в базе данных в зашифрованном виде и расшифровывается только при необходимости пройти реавторизацию. Шифрование осуществляется на основе алгоритма AES256.  
+
+Зачем хранить пароли?
+Токены сессии почтового сервера имеют весьма короткий срок жизни. При отсутствии обращений на почтовый сервер токен становится недействительным в течение 3-5 минут. К сожалению, бауманский почтовый сервер не имеет необходимый уровень доступности. Периодически он становится недоступен на промежутки времени, большие 5 минут. После такого периода недоступности все активные сессии становятся недействительны и требуется повторная авторизация. Для выполнения повторной авторизации в автоматическом режиме бот может использовать запомненные пароли пользователей.
+
+Если я хочу удалить свои данные из бота?
+Для удаления данных о пользователе можно воспользоваться командой /stop 
+
+Версия: `{}-{}`
 """
 LOGIN_WRONG_FORMAT_PROMPT = (
     "Неверный формат использования команды:\n/login <i>логин</i> <i>пароль</i>"
 )
 WAIT_TO_AUTH_PROMPT = "Авторизация. Пожалуйста, подождите..."
+SAVE_PASSWORD_PROMPT = "Авторизация выполнена. Сохранить пароль? Подробнее о хранении и использовании паролей: /about"
+PASSWORD_SAVED_PROMPT = "Пароль сохранен. Удалить пароль можно при помощи команды /stop"
 
 MAX_TELEGRAM_MESSAGE_LENGTH = 4096
+
+SAVE_PSW_CALLBACK = "SAVE_PSW"
+NO_SAVE_PSW_CALLBACK = "NO_SAVE_PSW"
 
 
 class TelegramBot:
@@ -45,6 +61,24 @@ class TelegramBot:
         ]
         self.handlers: list[ClientHandler] = []
 
+    async def callback_query_handler(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ):
+        data = update.callback_query.data.split(":")
+        command = data[0]
+        if command == SAVE_PSW_CALLBACK:
+            password = data[1]
+            self.db.set_password(update.effective_user.id, password)
+            await self.send_message(
+                update.effective_user.id, PASSWORD_SAVED_PROMPT, MARKDOWN_FORMAT
+            )
+            log.info(f"passport for user {update.effective_user.id} is saved")
+        elif command == NO_SAVE_PSW_CALLBACK:
+            log.info(f"passport for user {update.effective_user.id} is not saved")
+        await self.application.bot.delete_message(
+            update.effective_chat.id, update.message.id
+        )
+
     async def start_bot(self) -> None:
         log.info("starting the bot...")
         log.info("loading handlers...")
@@ -57,10 +91,11 @@ class TelegramBot:
         log.info("handlers are loaded")
 
         log.info("connecting to telegram api...")
-        self.application = Application.builder().token(env.getTelegramToken()).build()
+        self.application = Application.builder().token(env.get_telegram_token()).build()
 
         for command, handler in self.commands:
             self.application.add_handler(CommandHandler(command, handler))
+        self.application.add_handler(CallbackQueryHandler(self.callback_query_handler))
 
         await self.application.initialize()
         await self.application.start()
@@ -90,7 +125,7 @@ class TelegramBot:
         telegram_id = update.effective_user.id
         self.db.remove_client(
             telegram_id
-        )  # TODO: не удалять запись, а удалять только контекст
+        )  # TODO: не удалять запись, а удалять только контекст и пароль
         await update.message.reply_markdown(STOP_PROMPT)
 
     async def login_command(
@@ -114,6 +149,24 @@ class TelegramBot:
         if new_handler is not None:
             await new_handler.start_handling()
             self.handlers.append(new_handler)
+            await self.application.bot.send_message(
+                update.effective_chat.id,
+                SAVE_PASSWORD_PROMPT,
+                parse_mode=MARKDOWN_FORMAT,
+                reply_markup=telegram.InlineKeyboardMarkup(
+                    [
+                        telegram.InlineKeyboardButton(
+                            text="Да",
+                            callback_data=":".join(
+                                SAVE_PSW_CALLBACK, samoware_password
+                            ),
+                        ),
+                        telegram.InlineKeyboardButton(
+                            text="Нет", callback_data=":".join(NO_SAVE_PSW_CALLBACK)
+                        ),
+                    ]
+                ),
+            )
         await self.application.bot.delete_messages(
             update.effective_chat.id, [update.effective_message.id, wait_message.id]
         )
@@ -122,7 +175,7 @@ class TelegramBot:
         self, update: Update, context: ContextTypes.DEFAULT_TYPE
     ) -> None:
         await update.message.reply_markdown(
-            ABOUT_PROMPT.format(env.getProfile(), env.getVersion())
+            ABOUT_PROMPT.format(env.get_profile(), env.get_version())
         )
 
     async def send_message(
