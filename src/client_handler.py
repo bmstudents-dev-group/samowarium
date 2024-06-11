3759662cb7211074437f85e6fc48086a0f0e9ec1
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2024-06-11 16:24:32 +0300

    fix exception logging (#57)
    * fix exception logging
    
    * fix flake8 errors

--- src/client_handler.py
+++ src/client_handler.py
@@ -144,8 +144,8 @@ class ClientHandler:
                     )
                     retry_count += 1
                     await asyncio.sleep(HTTP_RETRY_DELAY_SEC)
-                except Exception as error:
-                    log.exception("exception in client_handler", error)
+                except Exception:
+                    log.exception("exception in client_handler")
                     log.warning(
                         f"retry_count={retry_count}. Retrying longpolling for {self.context.samoware_login} in {HTTP_RETRY_DELAY_SEC} seconds..."
                     )
@@ -177,10 +177,9 @@ class ClientHandler:
             except asyncio.CancelledError:
                 log.info("login cancelled")
                 return False
-            except Exception as e:
+            except Exception:
                 log.exception(
-                    f"retry_count={retry_count}. exception on login. retrying in {HTTP_RETRY_DELAY_SEC}...",
-                    e,
+                    f"retry_count={retry_count}. exception on login. retrying in {HTTP_RETRY_DELAY_SEC}..."
                 )
                 retry_count += 1
                 await asyncio.sleep(HTTP_RETRY_DELAY_SEC)
@@ -203,8 +202,8 @@ class ClientHandler:
             self.db.set_handler_context(self.context)
             log.info(f"successful revalidation for user {self.context.samoware_login}")
             return True
-        except Exception as e:
-            log.exception("exception on revalidation", e)
+        except Exception:
+            log.exception("exception on revalidation")
             return False
 
     async def can_not_revalidate(self):
